c66c997f216ae2f41d85b8079048e7a52e563d94
vinay thallam <vinay.thallam@jifflenow.com>
2018-04-16 20:52:19 +0530

    explicitly depend on lodash library and run webpack binary under node_modules/.bin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-//This file has an implicit dependency on lodash. 
-//i.e it assumes _ to be available before this code executes
+import _ from 'lodash'
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
