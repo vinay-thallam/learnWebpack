c35ba6ceec926d09b71a6b9bd43d2740108b8881
vinay thallam <vinay.thallam@jifflenow.com>
2018-09-16 20:19:37 +0530

    checking if the config works if CSS file contains base64 images - It Worksgit st

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('red')
+  element.classList.add('check')
   
   return element;
 }
