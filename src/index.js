5676228403c0631bad4851470d533a1866271e1a
vinay thallam <vinay.thallam@jifflenow.com>
2018-09-16 13:44:54 +0530

    build css dependency using css-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash'
+import './style.css'
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('red')
+  
   return element;
 }
 
