06053017fce82892ef513546868f2d770be6d66d
vinay thallam <vinay.thallam@jifflenow.com>
2018-09-16 14:06:03 +0530

    use extract text plugin to separate css into own output file - style loader cant be used as our css grows in size

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 
+var extractTextPlugin = new ExtractTextPlugin({filename: 'main.css'})
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [extractTextPlugin],
   module: {
   	rules: [{
   		test: /\.css$/,
-  		use: ['style-loader', 'css-loader']
+  		use: extractTextPlugin.extract({
+  			fallback: 'style-loader',
+  			use: ['css-loader']	
+  		})
+  		
   	}]
   }
 };
