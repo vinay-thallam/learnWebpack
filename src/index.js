55dca43c1f33f15422f2c95df676f260f3c22ee8
vinay thallam <vinay.thallam@jifflenow.com>
2018-09-16 20:37:50 +0530

    checking if the same asset names cause conflicts in the output directory. It doesnt git st Thanks to [hash]

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import _ from 'lodash'
 import 'style.css'
 
-function component() {
+function component(className) {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('check')
+  element.classList.add(className)
   
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component('red'));
+document.body.appendChild(component('same-icon-name'));
 
