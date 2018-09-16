0256f1c7405905962f57c31353c4a98e0a6fcb0e
vinay thallam <vinay.thallam@jifflenow.com>
2018-09-16 20:08:31 +0530

    specified public path in config, specified the image and font output names in url-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import './style.css'
+import 'style.css'
 
 function component() {
   var element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
+const ASSET_PATH = process.env.ASSET_PATH || '';
 
 var extractTextPlugin = new ExtractTextPlugin({filename: 'main.css'})
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: ASSET_PATH
   },
-  plugins: [extractTextPlugin],
+  plugins: [
+    extractTextPlugin,
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+    })
+  ],
+  resolve: {
+    modules: [
+      path.resolve(__dirname),
+      'node_modules',
+      'stylesheets'
+    ],
+  },
   module: {
   	rules: [{
   		test: /\.css$/,
@@ -17,21 +32,23 @@ module.exports = {
   			use: ['css-loader']	
   		})
   	},{
-		test: /\.(jpg|png|svg|gif)$/,
-		use: [{
-			loader: 'url-loader',
-			options: {
-				limit: 8192
-			}
-		}]
+  		test: /\.(jpg|png|svg|gif)$/,
+  		use: [{
+  			loader: 'url-loader',
+  			options: {
+  				limit: 8192,
+          name: 'images/[name]-[hash].[ext]'
+  			}
+  		}],
   	},{
-		test: /\.(woff|woff2|eot|ttf|otf)$/,
-		use: [{
-			loader: 'url-loader',
-			options: {
-				limit: 8192
-			}
-		}]
+  		test: /\.(woff|woff2|eot|ttf|otf)$/,
+  		use: [{
+  			loader: 'url-loader',
+  			options: {
+  				limit: 8192,
+          name: 'fonts/[name]-[hash].[ext]'
+  			}
+  		}]
   	}]
   }
 };
